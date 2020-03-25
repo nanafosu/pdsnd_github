16b79da4d2dcb93209272c3ddca75d4fa4bc5e23
Kwadwo Fosu Appiah <kfosu.appiah@gmail.com>
2020-03-25 22:13:08 +0000

    Edit line comments

--- bikeshare.py
+++ bikeshare.py
@@ -17,6 +17,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+
 # TO DO: get user input for city (chicago, new york city, washington)
     print("Would you like to see data from:")
     print("1. Chicago")
@@ -40,6 +41,7 @@ def get_filters():
         except ValueError:
             print("Invalid Option. Enter 1-3")
     options = ["both", "month", "day"]
+
 # TO DO: Print out the options
     for i in range(len(options)):
         print(str(i+1) + ":", options[i])
@@ -55,6 +57,7 @@ def get_filters():
                     continue
                 else:
                     break
+
 # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
             week_days=[1,2,3,4,5,6,7]
             while True:
@@ -68,6 +71,7 @@ def get_filters():
                 except ValueError:
                     print("Not an integer! Try again.")
                     continue
+
 # TO DO: get user input for month (all, january, february, ... , june)
         elif inp==2:
             months = ['january', 'february', 'march', 'april', 'may', 'june']
@@ -78,8 +82,9 @@ def get_filters():
                 else:
                     break
             day='all'
-        elif inp==3:
+
 # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+        elif inp==3:
             week_days=[1,2,3,4,5,6,7]
             while True:
                 try:
@@ -149,9 +154,11 @@ def time_stats(df):
 # TO DO: display the most common month
     most_common_month = df['month'].mode()[0]
     print("Most common month", most_common_month)
+
 # TO DO: display the most common day of week
     most_common_weekday=df['day_of_week'].mode()[0]
     print("Most common weekday:", most_common_weekday)
+
 # TO DO: display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
     most_common_hour=df['hour'].mode()[0]
@@ -172,10 +179,12 @@ def station_stats(df):
 # TO DO: display most commonly used end station
     most_common_endtime=df['End Time'].mode()[0]
     print("Most common End Time:", most_common_endtime)
+
 # TO DO: display most frequent combination of start station and end station trip
     freq_startendtrip_combination = df[['Start Station','End Station']].mode().loc[0]
     print("Most frequent combination of start station and end station trip:", freq_startendtrip_combination)
-    print("\nThis took %s seconds." % (time.time() - start_time))
+   
+   print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
@@ -197,6 +206,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 
+# TO DO: Compute statistical data
 def user_stats(df,city):
     """Displays statistics on bikeshare users."""
 
@@ -213,17 +223,20 @@ def user_stats(df,city):
     else:
         gender_type=df['Gender'].value_counts()
         print("Counts of gender:\n", gender_type)
-        # TO DO: Display earliest, most recent, and most common year of birth
+
+# TO DO: Display earliest, most recent, and most common year of birth
         birth_year=max(df['Birth Year'].mode())
         print("The earliest, most recent and most common year of birth: ", birth_year)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
     
-    
+  
 def display_raw_data(df):
     choice_yes=['yes','y','Yes', 'YES','Y']
     choice_no=['no','No','NO','n','N']
+
+# TO DO: Display individual raw data by user
     while True:
         view=input("Would you like to view individual data?\nyes or no:" )
         if view in choice_yes:
@@ -238,7 +251,7 @@ def display_raw_data(df):
     return rand_data
     
     
-# TO DO: Call all other functions
+# TO DO: Call all other functions to interact with user
 def main():
     while True:
         city, month, day = get_filters()
@@ -250,7 +263,7 @@ def main():
         user_stats(df,city)
         display_raw_data(df)
         
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart the analysis? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
